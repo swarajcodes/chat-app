bc72a61a86047de4fc4a010dc9c1ff94d3afb38c
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2025-07-13 23:27:31 +0530

    online users wala part hua and others.. next messaging

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -6,15 +6,29 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:["http://localhost:5173"]
+const io = new Server(server, {
+  cors: ["http://localhost:5173"],
 });
+
+export function getRecieverSocketId(userId) {
+  return userSocketMap[userId];
+}
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log(`User Connected : ${socket.id}`);
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log(`User disconnected : ${socket.id}`);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
